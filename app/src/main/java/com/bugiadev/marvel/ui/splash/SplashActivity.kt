71059d855cf30dd18358775be57cad9b931b39b2
kiko bugia <enrique.otero@servexternos.gruposantander.com>
2021-08-16 13:49:12 +0200

    [ADD] An awesome ironman splash has been added

---
+++ app/src/main/java/com/bugiadev/marvel/ui/splash/SplashActivity.kt
@@ -0,0 +1,56 @@
+package com.bugiadev.marvel.ui.splash
+
+import android.animation.Animator
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.bugiadev.marvel.R
+import com.bugiadev.marvel.databinding.ActivitySplashBinding
+import com.bugiadev.marvel.ui.main.MainActivity
+import java.lang.IllegalStateException
+
+class SplashActivity : AppCompatActivity() {
+    private var _binding: ActivitySplashBinding? = null
+    private var binding: ActivitySplashBinding
+        get() = _binding
+            ?: throw IllegalStateException("Binding is null, check the status of your Activity ${this::class}")
+        set(value) {
+            _binding = value
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_splash
+        )
+
+        binding.splashAnimation.addAnimatorListener(object : Animator.AnimatorListener {
+            override fun onAnimationRepeat(animation: Animator?) {
+                //Do nothing
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                Intent(applicationContext, MainActivity::class.java).apply {
+                    startActivity(this)
+                    finish()
+                }
+            }
+
+            override fun onAnimationCancel(animation: Animator?) {
+                //Do nothing
+            }
+
+            override fun onAnimationStart(animation: Animator?) {
+                //Do nothing
+            }
+
+        })
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
+}
